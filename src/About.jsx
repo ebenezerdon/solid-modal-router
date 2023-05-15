5e1d6ae34d24b0ad7fa9653cd567fe03508a5d7b
ebenezerdon <ebenezerdonu@gmail.com>
2023-05-15 10:58:54 +0100

    set up solid app with modal and router

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+const About = () => {
+  return (
+    <div>
+      <h1 class={styles.About}>About us!</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Home.jsx
@@ -0,0 +1,29 @@
+import { createSignal } from 'solid-js'
+import styles from './App.module.css'
+
+const Home = () => {
+  const [modalIsOpen, setModalIsOpen] = createSignal(false)
+
+  const toggleModal = () => {
+    setModalIsOpen(!modalIsOpen())
+  }
+
+  return (
+    <div class={styles.Home}>
+      <h1>Hello Motion One!</h1>
+      <button class={styles.openButton} onClick={() => toggleModal()}>
+        {modalIsOpen() ? 'Close Modal' : 'Open Modal'}
+      </button>
+
+      <Show when={modalIsOpen()}>
+        <div class={styles.Modal}>
+          <div>
+            <h1>Hello World!</h1>
+          </div>
+        </div>
+      </Show>
+    </div>
+  )
+}
+
+export default Home
